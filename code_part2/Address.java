0b59d7c3221af6d1cab9d2d5dea316958e9e1d90
dualthreatqb <rlica099@uottawa.ca>
2022-10-09 21:48:26 -0400

    Part 2 Code Commeted
    Commented part 2 code with descriptions.

--- code_part2/Address.java
+++ code_part2/Address.java
@@ -5,12 +5,23 @@ public class Address {
 	private int number;
 	private String postalCode;
 	
+	/*
+	 * Constructor function for an Address object. Creates an object of type Address.
+	 * 
+	 * Parameters: Street Name, Street Number and Postal Code
+	 */
 	public Address(String street, int number, String postalCode) {
 		this.street = street;
 		this.number = number;
 		this.postalCode = postalCode;
 	}
 	
+	/*
+	 * This method returns the Address in a written form easy to understand in basic english.
+	 * 
+	 * Returns: A string that reads out the address including the street name, number and postal code.
+	 * E.g 280 Somerset St E, K1N6V8
+	 */
 	public String writeAddress() {
 		return number + " " + street + ", " + postalCode;
 	}
--- code_part2/Test.java
+++ code_part2/Test.java
@@ -1,8 +1,23 @@
 package seg2105Assignment2;
 import java.util.ArrayList;  
 
+/*
+ * Author: Ryan Licandro
+ * Date: October 9th, 2022
+ * Course: SEG 2105
+ * 
+ * This program is a java object-oriented programming representation of the memory diagram given in part 2 of Assignment 1.
+ */
 public class Test {
 
+	/*
+	 * This main method is built to test the functionality of the program that models 
+	 * the memory diagram given in part 2 of Assignment 1.
+	 * 
+	 * Main method creates Falcao as an Employee with 2 unique addresses and displays his information.
+	 * 
+	 * Parameters: String[] args
+	 */
 	public static void main(String[] args) {
 		Address address1 = new Address("Queen", 48, "K1P1N2");
 		Address address2 = new Address("King Edward", 800, "K1N6N5");
